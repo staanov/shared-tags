2abdcc49357598639779f0e6647037f327a314fb
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2023-10-15 21:35:27 +0300

    Add backend basic structure

---
+++ backend/tags.shared/src/test/java/io/github/staanov/tags/shared/SharedTagsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.staanov.tags.shared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedTagsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
