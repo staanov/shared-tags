554443606151a968bfb52f81770629b918de3ec5
Stanislav Novikov <stanislav.a.novikov@gmail.com>
2025-02-16 16:48:37 +0300

    Add empty Spring Boot project for backend part

---
+++ shared-tags-back/shared-tags-back/src/main/java/io/github/staanov/shared_tags_back/SharedTagsBackApplication.java
@@ -0,0 +1,13 @@
+package io.github.staanov.shared_tags_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedTagsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedTagsBackApplication.class, args);
+	}
+
+}
---
+++ shared-tags-back/shared-tags-back/src/test/java/io/github/staanov/shared_tags_back/SharedTagsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.staanov.shared_tags_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedTagsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
